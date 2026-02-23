cf3bdd490ad832f960537ae816b170cf73912c86
xkarina2197 <xkarina2197@gmail.com>
2026-02-23 16:21:09 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,63 @@
+
+# 11-misol
+def ism(a = "xudoyor"):
+    print(f"salom {a}")
+
+ism()
+
+# 12-misol
+def raqam(a = 16):
+    print(a)
+
+raqam()
+
+# 13-misol
+def qoshish(a = 15, b = 16):
+    print(a + b)
+
+qoshish()
+
+
+# 14-misol
+def ismfam(a = 'Xudoyor', b = 16):
+    print(f"{a}{b} yoshda")
+
+ismfam()
+
+# 15-misol
+def kvadrat(a = 16):
+    print(f"16 ning kvadrati {a*a}")
+
+kvadrat()
+
+# 16-misol
+def takror(a = 'xudoyor '):
+    print(a*3)
+
+takror()
+
+# 17-misol
+def farq(a = 40, b = 15):
+    print(f"40 va 15ning farqi: {a-b}")
+
+farq()
+
+
+# 18-misol
+def qoshimcha(a= 'XUDOYBERGAN'):
+    print(f"Hurmatli {a}")
+
+qoshimcha()
+
+# 19-misol
+def kopaytirish(a = 15, b = 3):
+    print(f"15 va 3 ning kopaytmasi {a*b}")
+
+kopaytirish()
+
+
+# 20-misol
+def bolish(a = 48, b = 3):
+    print(f"48 va 3 ning bolinmasi: {a//b}")
+
+bolish()
